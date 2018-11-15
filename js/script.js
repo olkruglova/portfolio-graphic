174355290ae89e8083785ec2b6c2f0d2a5530835
olkruglova <olkruhlova@gmail.com>
2018-11-15 23:09:14 +0100

    Add lightgallery

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-$('.bw').mouseover(function() {
-    $(this).removeClass('bw');
-})
+$(window).on('load', function(){
+    $('html body').scrollTop(0);
+});
 
-$('.bw').mouseleave(function() {
-    $(this).addClass("bw");
-})
+$(document).ready(function() {
+    $("#images").lightGallery({
+        mode: 'lg-fade',
+        cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+    });
 
-$('.image').on('click', function(){
-    $('#pop-up').show()
-})
+    $('.bw').mouseover(function() {
+        $(this).removeClass('bw');
+    })
+
+    $('.bw').mouseleave(function() {
+        $(this).addClass("bw");
+    })
+
+    $('.image').on('click', function(){
+        $('#pop-up').show()
+    })
+});
